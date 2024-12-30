7819783a2d65cf885c077453b4b788fa6c0d9f67
Jeff Bothel <16275077+JeffBothel@users.noreply.github.com>
2024-12-29 21:55:01 -0700

    Additional Module 5 Files Additional files that were added to the Module 5 project. These files were added to the project to help with the implementation of the Google Maps API. The files include the following:
    Changes to be committed:
    	new file:   Module5/.gitignore
    	new file:   Module5/.idea/.gitignore
    	new file:   Module5/.idea/.name
    	new file:   Module5/.idea/codeStyles/Project.xml
    	new file:   Module5/.idea/codeStyles/codeStyleConfig.xml
    	new file:   Module5/.idea/compiler.xml
    	new file:   Module5/.idea/deploymentTargetSelector.xml
    	new file:   Module5/.idea/gradle.xml
    	new file:   Module5/.idea/kotlinc.xml
    	new file:   Module5/.idea/migrations.xml
    	new file:   Module5/.idea/misc.xml
    	new file:   Module5/.idea/runConfigurations.xml
    	new file:   Module5/.idea/vcs.xml
    	new file:   Module5/app/.gitignore
    	new file:   Module5/app/build.gradle.kts
    	new file:   Module5/app/proguard-rules.pro
    	new file:   Module5/app/src/androidTest/java/com/example/module5_gps/ExampleInstrumentedTest.kt
    	new file:   Module5/app/src/main/AndroidManifest.xml
    	new file:   Module5/app/src/main/java/com/example/module5_gps/MainActivity.kt
    	modified:   Module5/app/src/main/java/com/example/module5_gps/MapsActivity.kt
    	new file:   Module5/app/src/main/res/drawable/ic_launcher_background.xml
    	new file:   Module5/app/src/main/res/drawable/ic_launcher_foreground.xml
    	new file:   Module5/app/src/main/res/layout/activity_main.xml
    	modified:   Module5/app/src/main/res/layout/activity_maps.xml
    	new file:   Module5/app/src/main/res/layout/current_place_menu.xml
    	new file:   Module5/app/src/main/res/mipmap-anydpi/ic_launcher.xml
    	new file:   Module5/app/src/main/res/mipmap-anydpi/ic_launcher_round.xml
    	new file:   Module5/app/src/main/res/mipmap-hdpi/ic_launcher.webp
    	new file:   Module5/app/src/main/res/mipmap-hdpi/ic_launcher_round.webp
    	new file:   Module5/app/src/main/res/mipmap-mdpi/ic_launcher.webp
    	new file:   Module5/app/src/main/res/mipmap-mdpi/ic_launcher_round.webp
    	new file:   Module5/app/src/main/res/mipmap-xhdpi/ic_launcher.webp
    	new file:   Module5/app/src/main/res/mipmap-xhdpi/ic_launcher_round.webp
    	new file:   Module5/app/src/main/res/mipmap-xxhdpi/ic_launcher.webp
    	new file:   Module5/app/src/main/res/mipmap-xxhdpi/ic_launcher_round.webp
    	new file:   Module5/app/src/main/res/mipmap-xxxhdpi/ic_launcher.webp
    	new file:   Module5/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.webp
    	new file:   Module5/app/src/main/res/values-night/themes.xml
    	new file:   Module5/app/src/main/res/values/colors.xml
    	new file:   Module5/app/src/main/res/values/strings.xml
    	new file:   Module5/app/src/main/res/values/themes.xml
    	new file:   Module5/app/src/main/res/xml/backup_rules.xml
    	new file:   Module5/app/src/main/res/xml/data_extraction_rules.xml
    	new file:   Module5/app/src/test/java/com/example/module5_gps/ExampleUnitTest.kt
    	new file:   Module5/build.gradle.kts
    	new file:   Module5/gradle.properties
    	new file:   Module5/gradle/libs.versions.toml
    	new file:   Module5/gradle/wrapper/gradle-wrapper.jar
    	new file:   Module5/gradle/wrapper/gradle-wrapper.properties
    	new file:   Module5/gradlew
    	new file:   Module5/gradlew.bat
    	new file:   Module5/settings.gradle.kts

--- Module5/app/src/main/java/com/example/module5_gps/MapsActivity.kt
+++ Module5/app/src/main/java/com/example/module5_gps/MapsActivity.kt
@@ -13,19 +13,28 @@ import com.example.module5_gps.databinding.ActivityMapsBinding
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+
+
     private lateinit var mMap: GoogleMap
-    private lateinit var binding: ActivityMapsBinding
+private lateinit var binding: ActivityMapsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMapsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+     binding = ActivityMapsBinding.inflate(layoutInflater)
+     setContentView(R.layout.activity_maps)
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
+                .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        // Construct a PlacesClient
+        Places.initialize(applicationContext, getString(R.string.maps_api_key))
+        placesClient = Places.createClient(this)
+
+        // Construct a FusedLocationProviderClient.
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
     /**
@@ -37,6 +46,19 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
+    override fun onMapReady(map: GoogleMap) {
+        this.map = map
+
+        // ...
+
+        // Turn on the My Location layer and the related control on the map.
+        updateLocationUI()
+
+        // Get the current location of the device and set the position of the map.
+        getDeviceLocation()
+    }
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
@@ -44,5 +66,71 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    private fun getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.applicationContext,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        locationPermissionGranted = false
+        when (requestCode) {
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    locationPermissionGranted = true
+                }
+            }
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+        updateLocationUI()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (locationPermissionGranted) {
+                val locationResult = fusedLocationProviderClient.lastLocation
+                locationResult.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Set the map's camera position to the current location of the device.
+                        lastKnownLocation = task.result
+                        if (lastKnownLocation != null) {
+                            map?.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                LatLng(lastKnownLocation!!.latitude,
+                                    lastKnownLocation!!.longitude), DEFAULT_ZOOM.toFloat()))
+                        }
+                    } else {
+                        Log.d(TAG, "Current location is null. Using defaults.")
+                        Log.e(TAG, "Exception: %s", task.exception)
+                        map?.moveCamera(CameraUpdateFactory
+                            .newLatLngZoom(defaultLocation, DEFAULT_ZOOM.toFloat()))
+                        map?.uiSettings?.isMyLocationButtonEnabled = false
+                    }
+                }
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
     }
 }
